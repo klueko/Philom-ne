13b5584256435dd22a9ba7ad70c313ba9353b061
Kdsovm <93712810+Kdsovm@users.noreply.github.com>
2025-01-31 10:38:24 +0100

    Request with encodage

--- data-call/request.py
+++ data-call/request.py
@@ -8,8 +8,8 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
-    with open("reponse.json", "w") as f:
-        json.dump(data, f, indent=4)
+    with open("reponse.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
     print("Réponse sauvegardée dans reponse.json")
 else:
